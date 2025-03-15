f6f2afce37f32a4065f4603ed39dd47c6f5910e0
Hoang Bach Nguyen <hngu0996@uni.sydney.edu.au>
2025-03-15 16:12:10 +0700

    create file

---
+++ task1.py
@@ -0,0 +1,7 @@
+def connect_four_mm(contents, turn, max_depth):
+    #TODO
+    return ''
+
+if __name__ == '__main__':
+    # Example function call below, you can add your own to test the connect_four_mm function
+    connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1)
---
+++ task2.py
@@ -0,0 +1,7 @@
+def connect_four_ab(contents, turn, max_depth):
+    #TODO
+    return ''
+
+if __name__ == '__main__':
+    # Example function call below, you can add your own to test the connect_four_mm function
+    print(connect_four_ab(".ryyrry,.rryry.,..y.r..,..y....,.......,.......", "red", 4))

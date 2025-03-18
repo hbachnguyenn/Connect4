f978ee9d144bccf224ab8ea688fe215ac09b833c
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-03-18 17:25:17 +1100

    eval

--- utils/score.py
+++ utils/score.py
@@ -1,5 +1,6 @@
 import math
 
+
 def count_consecutive_moves_in_line(contents: [bool]):
     red = [0, 0, 0, 0]
     yellow = [0, 0, 0, 0]
@@ -31,6 +32,7 @@ def count_consecutive_moves_in_line(contents: [bool]):
 
     return red, yellow
 
+
 def calculate_score_in_line(red: [int], yellow: [int]):
     i = 0
     red_score = 0
@@ -41,3 +43,57 @@ def calculate_score_in_line(red: [int], yellow: [int]):
         yellow_score += yellow[i] * multiplier
         i += 1
     return red_score, yellow_score
+
+
+MAPPING = {'R': 1, 'Y': 2}
+
+
+def parse_board(board):
+    board = board.split(',')
+    for i in range(len(board)):
+        ls = list(board[i])
+        for j in range(len(ls)):
+            if ls[j] == 'r':
+                ls[j] = 1
+            elif ls[j] == 'y':
+                ls[j] = 2
+            else:
+                ls[j] = 0
+        board[i] = ls
+    return board
+
+
+def is_valid(row, col):
+    return 0 <= row < 6 and 0 <= col < 7
+
+
+def initial_board_evaluation(board, player):
+    directions = [(0, 1), (1, 0), (1, 1), (1, -1)]  # right, down, diag top right, diag top left
+    rows, cols = len(board), len(board[0])
+    counts = {2: 0, 3: 0, 4: 0}
+
+    for row in range(rows):
+        for col in range(cols):
+            if board[row][col] != player:
+                continue
+
+            for dr, dc in directions:
+                # either left, down, diag bot l r is the same player then alr counted
+                prev_r, prev_c = row - dr, col - dc
+                if is_valid(prev_r, prev_c) and board[prev_r][prev_c] == player:
+                    continue
+
+                length = 1
+                r, c = row + dr, col + dc
+                while is_valid(r, c) and board[r][c] == player:
+                    length += 1
+                    r += dr
+                    c += dc
+
+                if length >= 4:
+                    counts[4] += 1
+                elif length == 3:
+                    counts[3] += 1
+                elif length == 2:
+                    counts[2] += 1
+    return counts

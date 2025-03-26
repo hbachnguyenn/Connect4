f876511866e6680e74f2f0b7875cfbd32d9e5863
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-03-26 20:48:57 +1100

    update bug

--- task1.py
+++ task1.py
@@ -9,7 +9,7 @@ def connect_four_mm(contents, turn, max_depth):
     return f"{e.get_col()}\n{tree.nodes_examined}"
 
 if __name__ == '__main__':
-    # result = connect_four_mm("yrry.ry,yrr......,y......,.......,.......,.......", "yellow", 4)
-    result = connect_four_mm("yyy....,.......,.......,.......,.......,.......", "red", 2)
+    result = connect_four_mm("yrry.ry,yrr......,y......,.......,.......,.......", "yellow", 4)
+    # result = connect_four_mm("yyy....,.......,.......,.......,.......,.......", "red", 2)
     print("\nResult:")
     print(result)

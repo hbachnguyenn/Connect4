51ab90fd56ff287346abb0b0f74dcb0b286494d1
Hoang Bach Nguyen <hoangbach.nguyen215@gmail.com>
2025-03-27 19:01:48 +1100

    Delete unneecessary output

--- task1.py
+++ task1.py
@@ -3,13 +3,10 @@ from utils.tree import Tree
 def connect_four_mm(contents, turn, max_depth):
     tree = Tree(contents, turn, max_depth)
     tree.generate_state_space()
-    tree.display_tree(tree.root)
 
     e = tree.minimax(tree.root, 0, max_depth, tree.root.turn)
     return f"{e.get_col()}\n{tree.nodes_examined}"
 
 if __name__ == '__main__':
-    result = connect_four_mm("yrry.ry,yrr......,y......,.......,.......,.......", "yellow", 4)
-    # result = connect_four_mm("yyy....,.......,.......,.......,.......,.......", "red", 2)
-    print("\nResult:")
+    result = connect_four_mm("yyy....,.......,.......,.......,.......,.......", "red", 2)
     print(result)
--- task2.py
+++ task2.py
@@ -1,7 +1,12 @@
-def connect_four_ab(contents, turn, max_depth):
-    #TODO
-    return ''
+from utils.tree import Tree
+
+def connect_four_mm(contents, turn, max_depth):
+    tree = Tree(contents, turn, max_depth)
+    tree.generate_state_space()
+
+    e = tree.minimax(tree.root, 0, max_depth, tree.root.turn)
+    return f"{e.get_col()}\n{tree.nodes_examined}"
 
 if __name__ == '__main__':
-    # Example function call below, you can add your own to test the connect_four_mm function
-    print(connect_four_ab(".ryyrry,.rryry.,..y.r..,..y....,.......,.......", "red", 4))
+    result = connect_four_mm("yyy....,.......,.......,.......,.......,.......", "red", 2)
+    print(result)
